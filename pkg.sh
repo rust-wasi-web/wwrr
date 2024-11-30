3d0f2eef8dbb54f22ac5977a2ecec41efa71db31
Sebastian Urban <surban@surban.net>
2024-11-30 12:48:45 +0100

    Include wasm-bindgen CLI wrapper
    This ensures that wasm-bindgen version matches that
    of the wasm-bindgen tool run after building.

--- pkg.sh
+++ pkg.sh
@@ -5,6 +5,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 rm -rf pkg
 
-cargo build --release
-wasm-bindgen --target web --out-dir pkg target/wasm32-unknown-unknown/release/wwrr.wasm
+cargo build --release --package wwrr
+cargo run --quiet --release --target x86_64-unknown-linux-gnu --bin bindgen-cli -- --target web --out-dir pkg target/wasm32-unknown-unknown/release/wwrr.wasm
 rm -f pkg/wwrr_bg.wasm.d.ts
