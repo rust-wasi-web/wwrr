533d3583d7a37e0ed9894792c489a564adb1446b
Sebastian Urban <surban@surban.net>
2025-03-19 15:15:14 +0000

    prefix for wasm-bindgen log

--- pkg.sh
+++ pkg.sh
@@ -5,6 +5,8 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 rm -rf pkg
 
+export WBG_PKG=wwrr-
+
 cargo build --release --package wwrr
 cargo run --quiet --release --target x86_64-unknown-linux-gnu --bin bindgen-cli -- --target web --out-dir pkg target/wasm32-unknown-unknown/release/wwrr.wasm
 rm -f pkg/wwrr_bg.wasm.d.ts
